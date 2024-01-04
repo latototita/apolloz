b858297848df46dea0e29be226f3a4370d637f02
latototita <85679433+latototita@users.noreply.github.com>
2024-01-04 14:32:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask
-import script
 app = Flask(__name__)
+from script import run_trading_bot
+symbol='XAGUSDm'
+timeframe='1h'
+
 
 @app.route('/')
 def home():
+    run_trading_bot()
     return "I am alive"
 
 if __name__=="__main__":
-  os.system("python3 script.py &")
+  os.system("python script.py &")
   app.run(host="0.0.0.0", port=80)
