6f158bd932cb1fe95aceffc41fd5cf1833afb833
latototita <85679433+latototita@users.noreply.github.com>
2024-01-04 15:29:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
-from script import run_trading_bot
-symbol='XAGUSDm'
-timeframe='1h'
 
 
 @app.route('/')
 def home():
-    return "I am alive"
-@app.route('/index')
-def index():
-    print('Start')
-    run_trading_bot()
-    print('Stop')
     return "I am alive"
 
 if __name__=="__main__":
