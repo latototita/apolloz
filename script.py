dfafca3bf76acdab0f866d4b5d011808a9ae960b
latototita <85679433+latototita@users.noreply.github.com>
2024-01-08 12:53:43 -0600

    Update script.py

--- script.py
+++ script.py
@@ -96,7 +96,7 @@ def run_trading_bot():
                                     
                                     result = await connection.create_market_sell_order(
                                         symbol,
-                                        0.03,
+                                        0.01,
                                         stop_loss,
                                         take_profit,
                                         )
@@ -111,7 +111,7 @@ def run_trading_bot():
                                 try:
                                     result = await connection.create_market_buy_order(
                                         symbol,
-                                        0.03,
+                                        0.01,
                                         stop_loss,
                                         take_profit,
                                         )
