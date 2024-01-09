43010e72609a9d430b9ecf896d42f76958c87a7c
latototita <85679433+latototita@users.noreply.github.com>
2024-01-09 13:38:00 -0600

    Add files via upload

---
+++ apollo2.py
@@ -0,0 +1,131 @@
+import numpy as np
+from sklearn.svm import SVR
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+
+# Load the dataset
+
+def prediction_home(df):
+
+    # Use 'Close' column as the target variable
+    target_col = 'close'
+
+    # Feature scaling with MinMaxScaler
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(df[target_col].values.reshape(-1, 1))
+
+    # Set prediction days and create sequences
+    prediction_days = 180
+
+    x_train = []
+    y_train = []
+
+    for x in range(prediction_days, len(scaled_data)):
+        x_train.append(scaled_data[x - prediction_days:x, 0])
+        y_train.append(scaled_data[x, 0])
+
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    dt = x_train
+
+    # Split the data into training and testing sets
+    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.01, random_state=42, shuffle=False)
+    # Define the parameter grid
+    param_grid = {
+        'kernel': ['linear', 'rbf'],  # You can add more kernel options if needed
+        'C': [1,1.2,1.5,1.8,],  # Adjust these values based on your problem
+        'epsilon': [0.01, 0.1, 0.5]  # Adjust these values based on your problem
+    }
+
+    # Create the SVR model
+    svm_model = SVR()
+
+    # Create the GridSearchCV object
+    grid_search = GridSearchCV(svm_model, param_grid, scoring='neg_mean_squared_error', cv=5)
+
+    # Flatten the sequences for SVM
+    x_train_svm = x_train.reshape(x_train.shape[0], -1)
+    # Flatten the sequences for SVM
+    x_train_svm_flattened = x_train.reshape(x_train.shape[0], -1)
+
+    # Fit the grid search to the data
+    grid_search.fit(x_train_svm_flattened, y_train)
+
+    # Print the best parameters found by the grid search
+    print("Best Parameters:", grid_search.best_params_)
+
+    # Get the best SVM model
+    best_svm_model = grid_search.best_estimator_
+
+    # Make predictions on the test set
+    x_test_svm_flattened = x_test.reshape(x_test.shape[0], -1)
+    y_pred = best_svm_model.predict(x_test_svm_flattened)
+
+    """
+    x_train_svm = x_train.reshape(x_train.shape[0], -1)
+    # Build and train the SVM model
+    model = SVR(kernel='linear',C=0.1,epsilon=0.01)
+    model.fit(x_train_svm, y_train)
+
+    # Flatten the sequences for SVM
+    x_test_svm = x_test.reshape(x_test.shape[0], -1)
+
+    # Make predictions on the test set
+    y_pred = model.predict(x_test_svm)
+    """
+    # Inverse transform the predictions and true values to the original scale
+    y_pred_inv = scaler.inverse_transform(y_pred.reshape(-1, 1))
+    y_test_inv = scaler.inverse_transform(y_test.reshape(-1, 1))
+
+    # Calculate and print the mean squared error
+    mse = mean_squared_error(y_test_inv, y_pred_inv)
+    print(f'Mean Squared Error: {mse:.24f}')
+
+    # Calculate evaluation metrics
+    r_squared = r2_score(y_test_inv, y_pred_inv)
+    rmse = np.sqrt(mse)
+    mae = mean_absolute_error(y_test_inv, y_pred_inv)
+    mape = np.mean(np.abs((y_test_inv - y_pred_inv) / y_test_inv) * 100)
+
+    # Print evaluation metrics
+    print(f"R-squared: {r_squared:.24f}")
+    print(f"Root Mean Squared Error (RMSE): {rmse:.24f}")
+    print(f"Mean Absolute Error (MAE): {mae:.24f}")
+    print(f"Mean Absolute Percentage Error (MAPE): {mape:.24f}%")
+
+    # Predict Next Day
+    real_data = scaled_data[-prediction_days:]  # Assuming x_train is your input data
+    real_data_flattened = real_data.reshape(1, -1)  # Reshape for prediction
+
+    # Make predictions
+    prediction = best_svm_model.predict(real_data_flattened)
+
+    # Inverse transform the prediction to the original scale
+    prediction = scaler.inverse_transform(prediction.reshape(-1, 1))
+
+
+    # Identify sell and buy conditions
+    sell_condition = y_test_inv[:-1] > y_test_inv[1:]  # Close below the previous price
+    buy_condition = y_test_inv[:-1] < y_test_inv[1:]  # Close above the previous price
+
+    # Filter elements for buy and sell scenarios
+    sell_test = y_test_inv[:-1][sell_condition]
+    sell_pred = y_pred_inv[:-1][sell_condition]
+
+    buy_test = y_test_inv[:-1][buy_condition]
+    buy_pred = y_pred_inv[:-1][buy_condition]
+
+    # Calculate maximum absolute differences for buy and sell
+    min_difference_sell = np.abs(sell_test - sell_pred).min()
+    min_difference_buy = np.abs(buy_test - buy_pred).min()
+
+    return prediction[0].item(),min_difference_buy,min_difference_sell,mse
+"""
+file_path = 'EURUSDm_1h.csv'
+df=pd.read_csv(file_path)
+take_profit,min_difference_buy,min_difference_sell,mse=prediction_home(df)
+print(take_profit,min_difference_buy,min_difference_sell,mse)"""
---
+++ script2.py
@@ -0,0 +1,132 @@
+import pandas as pd
+import numpy as np
+import asyncio
+import numpy as np
+import pandas as pd
+import numpy as np
+import os
+from decimal import Decimal
+import pandas_ta as ta
+from metaapi_cloud_sdk import MetaApi
+import csv
+import time
+from apollo2 import prediction_home
+from get_candles import get_candles_m
+data_list=[]
+token = os.getenv('TOKEN') or 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2YjI0NTQ0ZWYzMWI0NzQ4NWMxNzQ1NmUzNzdmYTlhZiIsInBlcm1pc3Npb25zIjpbXSwidG9rZW5JZCI6IjIwMjEwMjEzIiwiaW1wZXJzb25hdGVkIjpmYWxzZSwicmVhbFVzZXJJZCI6IjZiMjQ1NDRlZjMxYjQ3NDg1YzE3NDU2ZTM3N2ZhOWFmIiwiaWF0IjoxNjg1NTM4OTg4fQ.K0bb-27iMrcf3gDYGylSgmf1KkcIgnLDL961KBHD3vuYwLC9funTPn-U7wBhvBUDN9pXwdwkBPoA19zIOiZLUxLcNWKcQD3i26TIdu9EhES1xnl1_dLfTPeDhN6SCHGZILh2fO331HexxRa0wqmOiUKYEZgLHSo9VXMCtFSgxJyqrhQzU35U76EWCKHI4yIYRAu8XSFR8RZ6GjeBgqI-J7Y--Z68ldAWisc2RKDUgFeo4ooillmrzTr73dr1usEn9APO25jeUGLm6Qkc8u8eox_vqSvFqovpZZ3czbR21-oEdqFT5EunGh-98WBND6IXfZlxDlBHJ-Ps7r1o9jm4A7vUPBFuGQ6MQ1dcUqKTNYA4p2DGA4lgB1kljoUQhPFau1QkgsJxc7KZExLs8Clg4aNybEO8SwP7uKt9V2UBDqRJT7ZUIrKKgz0uNisuPmS8ml5kKOKcZVQaAUvkbXJuI6vmKWVPeZdGEJu009W-tOuAvgiy2xgrtUpTFBgPAPciK-jrxiRdLHBTij40uYem0UhdmmlaUEH9FGnf9LpnVkvVTl7nrANf3g-yOI3yOAoBupZfAPucEGP8HVvZBfmwdu2GhAMs1cDDij49AUJEoBt1FDqYxOgIyvhGY5Baisn9FC_V-FROyKASzXz0A3cHZUZ63Vm9ghDsDA6rOJd1Kkk'
+accountId = os.getenv('ACCOUNT_ID') or 'bb7fcbbb-5eff-4805-92ca-2369aeb26ee3'
+symbol='EURUSDm'
+timeframe='1h'
+#symbol_list = ['XAGUSDm', 'EURUSDm', 'GBPUSDm', 'GBPTRYm', 'XAUUSDm', 'AUDCHFm', 'NZDUSDm', 'GBPCHFm', 'USDCHFm','XAUUSDm']
+#symbol_list = ['EURUSDm', 'GBPUSDm','GBPCHFm','USDCHFm','AUDCHFm',]
+
+def run_trading_bot():
+    async def main():
+        while True:
+            # Connect to the MetaTrader account
+            try:
+                # Fetch historical price data
+                candles = await get_candles_m(timeframe,symbol)
+                k=True
+                word1="Error"
+                while k==True:
+                    if type(candles)== str:
+                        if word1.lower() in candles.lower():
+                            candles =await get_candles_m(timeframe,symbol)
+                    else:
+                        k=False
+            except Exception as e:
+                print(f"Error retrieving candle data: {e}")  
+            df = pd.DataFrame(candles)
+            df=df.dropna()
+            # Convert 'time' to datetime and set it as the index
+            df['time'] = pd.to_datetime(df['time'])
+            df.set_index('time', inplace=True)
+            take_profit,min_difference_buy,min_difference_sell,mse=prediction_home(df)
+            #print(take_profit,min_difference_buy,min_difference_sell,mse)
+            current_price=df['close'].iloc[-1]
+
+            if float(mse)<=0.000001:
+                api = MetaApi(token)
+                account = await api.metatrader_account_api.get_account(accountId)
+                initial_state = account.state
+                deployed_states = ['DEPLOYING', 'DEPLOYED']
+                if initial_state not in deployed_states:
+                    # Wait until the account is deployed and connected to the broker
+                    print('Deploying account')
+                    await account.deploy()
+                print('Waiting for API server to connect to the broker (may take a few minutes)')
+                await account.wait_connected()
+
+                # Connect to MetaApi API
+                connection = account.get_rpc_connection()
+                await connection.connect()
+
+                # Wait until terminal state synchronized to the local state
+                print('Waiting for SDK to synchronize to terminal state (may take some time depending on your history size)')
+                await connection.wait_synchronized()
+                
+
+                trades = await connection.get_positions()#connection.get_orders()
+                if len(trades)>2:
+                    print(trades)
+                    print("There are open trades. Skipping analysis.")
+                    await asyncio.sleep(120)
+                else:
+                    trades = await connection.get_positions()
+
+                    if len(trades)>2:
+                        print("There are open trades. Skipping analysis.")
+                        await asyncio.sleep(120)
+                    
+                    print('passed 1')
+                    # Connect to MetaApi API
+                    
+                    if current_price>take_profit:
+                        take_profit=take_profit+min_difference_sell
+                        print('passed 2')
+                        stop_loss=current_price +((current_price-take_profit)*2)
+                        try:
+                            
+                            result = await connection.create_market_sell_order(
+                                symbol,
+                                0.03,
+                                stop_loss,
+                                take_profit,
+                                )
+                            print(f'Sell Signal (T)   :Sell Trade successful')
+                            
+                        except Exception as err:
+                            print('Trade failed with error:')
+                            print(api.format_error(err))
+                    elif current_price<take_profit:
+                        take_profit=take_profit-min_difference_buy
+                        print('passed 3')
+                        stop_loss=current_price -((take_profit-current_price)*2)
+                        try:
+                            result = await connection.create_market_buy_order(
+                                symbol,
+                                0.03,
+                                stop_loss,
+                                take_profit,
+                                )
+                            print(f'Buy_Signal (T)   :Buy Trade successful')
+                        except Exception as err:
+                            print('Trade failed with error:')
+                            print(api.format_error(err))
+
+                    
+                    
+                            
+                    current_trades = await connection.get_positions()
+                    if len(current_trades)>2:
+                        await asyncio.sleep(600)
+                    else:
+                        print("--------------------------------------------------")
+
+
+            await asyncio.sleep(300)  # Sleep for 1 minute before the next iteration
+    asyncio.run(main())
+
+
+run_trading_bot()
