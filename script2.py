951abbbdbda07684ff8bf3545fe719254c41541f
latototita <85679433+latototita@users.noreply.github.com>
2024-01-10 02:29:07 -0600

    Update script2.py

--- script2.py
+++ script2.py
@@ -16,7 +16,7 @@ data_list=[]
 token = os.getenv('TOKEN') or 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2YjI0NTQ0ZWYzMWI0NzQ4NWMxNzQ1NmUzNzdmYTlhZiIsInBlcm1pc3Npb25zIjpbXSwidG9rZW5JZCI6IjIwMjEwMjEzIiwiaW1wZXJzb25hdGVkIjpmYWxzZSwicmVhbFVzZXJJZCI6IjZiMjQ1NDRlZjMxYjQ3NDg1YzE3NDU2ZTM3N2ZhOWFmIiwiaWF0IjoxNjg1NTM4OTg4fQ.K0bb-27iMrcf3gDYGylSgmf1KkcIgnLDL961KBHD3vuYwLC9funTPn-U7wBhvBUDN9pXwdwkBPoA19zIOiZLUxLcNWKcQD3i26TIdu9EhES1xnl1_dLfTPeDhN6SCHGZILh2fO331HexxRa0wqmOiUKYEZgLHSo9VXMCtFSgxJyqrhQzU35U76EWCKHI4yIYRAu8XSFR8RZ6GjeBgqI-J7Y--Z68ldAWisc2RKDUgFeo4ooillmrzTr73dr1usEn9APO25jeUGLm6Qkc8u8eox_vqSvFqovpZZ3czbR21-oEdqFT5EunGh-98WBND6IXfZlxDlBHJ-Ps7r1o9jm4A7vUPBFuGQ6MQ1dcUqKTNYA4p2DGA4lgB1kljoUQhPFau1QkgsJxc7KZExLs8Clg4aNybEO8SwP7uKt9V2UBDqRJT7ZUIrKKgz0uNisuPmS8ml5kKOKcZVQaAUvkbXJuI6vmKWVPeZdGEJu009W-tOuAvgiy2xgrtUpTFBgPAPciK-jrxiRdLHBTij40uYem0UhdmmlaUEH9FGnf9LpnVkvVTl7nrANf3g-yOI3yOAoBupZfAPucEGP8HVvZBfmwdu2GhAMs1cDDij49AUJEoBt1FDqYxOgIyvhGY5Baisn9FC_V-FROyKASzXz0A3cHZUZ63Vm9ghDsDA6rOJd1Kkk'
 accountId = os.getenv('ACCOUNT_ID') or 'bb7fcbbb-5eff-4805-92ca-2369aeb26ee3'
 symbol='GBPUSDm'
-timeframe='4h'
+timeframe='1d'
 #symbol_list = ['XAGUSDm', 'EURUSDm', 'GBPUSDm', 'GBPTRYm', 'XAUUSDm', 'AUDCHFm', 'NZDUSDm', 'GBPCHFm', 'USDCHFm','XAUUSDm']
 #symbol_list = ['EURUSDm', 'GBPUSDm','GBPCHFm','USDCHFm','AUDCHFm',]
 
@@ -68,29 +68,23 @@ def run_trading_bot():
                 
 
                 trades = await connection.get_positions()#connection.get_orders()
-                if len(trades)>2:
+                if len(trades)>1:
                     print(trades)
                     print("There are open trades. Skipping analysis.")
                     await asyncio.sleep(120)
                 else:
-                    trades = await connection.get_positions()
-
-                    if len(trades)>2:
-                        print("There are open trades. Skipping analysis.")
-                        await asyncio.sleep(120)
-                    
                     print('passed 1')
                     # Connect to MetaApi API
                     
                     if current_price>take_profit:
-                        take_profit=take_profit+min_difference_sell
+                        #take_profit=take_profit+min_difference_sell
                         print('passed 2')
                         stop_loss=current_price +((current_price-take_profit)*3)
                         try:
                             
                             result = await connection.create_market_sell_order(
                                 symbol,
-                                0.03,
+                                0.05,
                                 stop_loss,
                                 take_profit,
                                 )
@@ -100,13 +94,13 @@ def run_trading_bot():
                             print('Trade failed with error:')
                             print(api.format_error(err))
                     elif current_price<take_profit:
-                        take_profit=take_profit-min_difference_buy
+                        #take_profit=take_profit-min_difference_buy
                         print('passed 3')
                         stop_loss=current_price -((take_profit-current_price)*3)
                         try:
                             result = await connection.create_market_buy_order(
                                 symbol,
-                                0.03,
+                                0.05,
                                 stop_loss,
                                 take_profit,
                                 )
@@ -119,7 +113,7 @@ def run_trading_bot():
                     
                             
                     current_trades = await connection.get_positions()
-                    if len(current_trades)>2:
+                    if len(current_trades)>1:
                         await asyncio.sleep(600)
                     else:
                         print("--------------------------------------------------")
